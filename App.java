1ce2c920905a9fd11208b8cb11cd2e1764000269
tm7890 <aids.61.tanmaymandal@gmail.com>
2025-03-17 23:17:16 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,13 +1,33 @@
-package com.example.seleniumtest;
+package com.example.tests;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void googleSearch() {
+        driver.get("https://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium WebDriver");
+        searchBox.submit();
+        System.out.println("Search performed successfully");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
     }
 }
